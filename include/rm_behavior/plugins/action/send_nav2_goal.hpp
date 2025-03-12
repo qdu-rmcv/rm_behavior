9c8260d7f1088fa34893945708f6b5c2d21d91b8
Jquarker <cjq1822261285@outlook.com>
2025-03-12 09:18:00 +0800

    初步测试成功

--- include/rm_behavior/plugins/action/send_nav2_goal.hpp
+++ include/rm_behavior/plugins/action/send_nav2_goal.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Jquark
+// Copyright 2025 Lihan Chen
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,39 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef RM_BEHAVIOR__PLUGINS__ACTION__NAV2GOAL_HPP_
-#define RM_BEHAVIOR__PLUGINS__ACTION__NAV2GOAL_HPP_
+#ifndef PB2025_SENTRY_BEHAVIOR__PLUGINS__ACTION__SEND_NAV2_GOAL_HPP_
+#define PB2025_SENTRY_BEHAVIOR__PLUGINS__ACTION__SEND_NAV2_GOAL_HPP_
 
 #include <memory>
 #include <string>
 
 #include "behaviortree_ros2/bt_action_node.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "rm_behavior/custom_types.hpp"
 
-namespace rm_behavior {
-class Nav2GoalAction
-    : public BT::RosActionNode<nav2_msgs::action::NavigateToPose> {
-
+namespace pb2025_sentry_behavior
+{
+class SendNav2GoalAction : public BT::RosActionNode<nav2_msgs::action::NavigateToPose>
+{
 public:
-  Nav2GoalAction(const std::string &name, const BT::NodeConfig &conf,
-             const BT::RosNodeParams &params);
+  SendNav2GoalAction(
+    const std::string & name, const BT::NodeConfig & conf, const BT::RosNodeParams & params);
 
   static BT::PortsList providedPorts();
 
-  bool setGoal(Goal &goal) override;
+  bool setGoal(Goal & goal) override;
 
   void onHalt() override;
 
-  BT::NodeStatus onResultReceived(const WrappedResult &wr) override;
+  BT::NodeStatus onResultReceived(const WrappedResult & wr) override;
 
-  BT::NodeStatus
-  onFeedback(const std::shared_ptr<const Feedback> feedback) override;
+  BT::NodeStatus onFeedback(const std::shared_ptr<const Feedback> feedback) override;
 
   BT::NodeStatus onFailure(BT::ActionNodeErrorCode error) override;
 };
-} // namespace rm_behavior
+}  // namespace pb2025_sentry_behavior
 
-#endif // RM_BEHAVIOR__PLUGINS__ACTION__NAV2GOAL_HPP_
+#endif  // PB2025_SENTRY_BEHAVIOR__PLUGINS__ACTION__SEND_NAV2_GOAL_HPP_
--- src/plugins/action/send_nav2_goal.cpp
+++ src/plugins/action/send_nav2_goal.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Jquark
+// Copyright 2025 Lihan Chen
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,19 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "rm_behavior/plugins/action/nav2goal.hpp"
-#include "rm_behavior/custom_types.hpp"
+#include "pb2025_sentry_behavior/plugins/action/send_nav2_goal.hpp"
 
-namespace rm_behavior {
+#include "pb2025_sentry_behavior/custom_types.hpp"
 
-Nav2GoalAction::Nav2GoalAction(const std::string &name,
-                             const BT::NodeConfig &conf,
-                             const BT::RosNodeParams &params)
+namespace pb2025_sentry_behavior
+{
+
+SendNav2GoalAction::SendNav2GoalAction(
+  const std::string & name, const BT::NodeConfig & conf, const BT::RosNodeParams & params)
 : RosActionNode<nav2_msgs::action::NavigateToPose>(name, conf, params)
 {
 }
 
-bool Nav2GoalAction::setGoal(nav2_msgs::action::NavigateToPose::Goal & goal)
+bool SendNav2GoalAction::setGoal(nav2_msgs::action::NavigateToPose::Goal & goal)
 {
   auto receive_goal = getInput<geometry_msgs::msg::PoseStamped>("goal");
 
@@ -35,54 +36,52 @@ bool Nav2GoalAction::setGoal(nav2_msgs::action::NavigateToPose::Goal & goal)
   return true;
 }
 
-BT::NodeStatus Nav2GoalAction::onResultReceived(const WrappedResult & wr)
+BT::NodeStatus SendNav2GoalAction::onResultReceived(const WrappedResult & wr)
 {
   switch (wr.code) {
     case rclcpp_action::ResultCode::SUCCEEDED:
-      RCLCPP_INFO(logger(), "导航成功!");
+      RCLCPP_INFO(logger(), "Navigation succeeded!");
       return BT::NodeStatus::SUCCESS;
 
     case rclcpp_action::ResultCode::ABORTED:
-      RCLCPP_ERROR(logger(), "导航被服务器中止");
+      RCLCPP_ERROR(logger(), "Navigation aborted by server");
       return BT::NodeStatus::FAILURE;
 
     case rclcpp_action::ResultCode::CANCELED:
-      RCLCPP_WARN(logger(), "导航被取消");
+      RCLCPP_WARN(logger(), "Navigation canceled");
       return BT::NodeStatus::FAILURE;
 
     default:
-      RCLCPP_ERROR(logger(), "未知的导航结果代码: %d", static_cast<int>(wr.code));
+      RCLCPP_ERROR(logger(), "Unknown navigation result code: %d", static_cast<int>(wr.code));
       return BT::NodeStatus::FAILURE;
   }
 }
 
-BT::NodeStatus Nav2GoalAction::onFeedback(
+BT::NodeStatus SendNav2GoalAction::onFeedback(
   const std::shared_ptr<const nav2_msgs::action::NavigateToPose::Feedback> feedback)
 {
-  RCLCPP_DEBUG(logger(), "剩余距离: %f", feedback->distance_remaining);
+  RCLCPP_DEBUG(logger(), "Distance remaining: %f", feedback->distance_remaining);
   return BT::NodeStatus::RUNNING;
 }
 
-void Nav2GoalAction::onHalt() { RCLCPP_INFO(logger(), "Nav2GoalAction 已被暂停."); }
+void SendNav2GoalAction::onHalt() { RCLCPP_INFO(logger(), "SendNav2GoalAction has been halted."); }
 
-BT::NodeStatus Nav2GoalAction::onFailure(BT::ActionNodeErrorCode error)
+BT::NodeStatus SendNav2GoalAction::onFailure(BT::ActionNodeErrorCode error)
 {
-  RCLCPP_ERROR(logger(), "Nav2GoalAction 失败，错误代码: %d", error);
+  RCLCPP_ERROR(logger(), "SendNav2GoalAction failed with error code: %d", error);
   return BT::NodeStatus::FAILURE;
 }
 
-BT::PortsList Nav2GoalAction::providedPorts()
+BT::PortsList SendNav2GoalAction::providedPorts()
 {
   BT::PortsList additional_ports = {
     BT::InputPort<geometry_msgs::msg::PoseStamped>(
-      "goal", "0;0;0", "导航目标位置，格式为 `x;y;yaw`"),
-    BT::InputPort<std::string>(
-      "action_client", "navigate_to_pose", "导航动作客户端名称"),
+      "goal", "0;0;0", "Expected goal pose that send to nav2. Fill with format `x;y;yaw`"),
   };
   return providedBasicPorts(additional_ports);
 }
 
-}  // namespace rm_behavior
+}  // namespace pb2025_sentry_behavior
 
 #include "behaviortree_ros2/plugins.hpp"
-CreateRosNodePlugin(rm_behavior::Nav2GoalAction, "Nav2Goal");
+CreateRosNodePlugin(pb2025_sentry_behavior::SendNav2GoalAction, "SendNav2Goal");
